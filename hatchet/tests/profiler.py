46be08ddc94550a9216b00415e0cb31a41a7a9d0
Connor Scully-Allison <cscully-allison@nevada.unr.edu>
2020-08-03 15:03:35 -0700

    Reversed changes from the previous commit and am uploading this as a tentative framework/design of the profiler class. Reinserted timer. Tests are just placeholders right now.

--- hatchet/tests/profiler.py
+++ hatchet/tests/profiler.py
@@ -1,6 +1,3 @@
-import pstats
-import cProfile
-import os
 from hatchet.util.profiler import Profiler
 
 
@@ -9,59 +6,15 @@ def f():
         for j in range(1000):
             i * j
 
-def debug():
-    prf = Profiler()
-
-    with prf.phase("test"):
-        f()
-
-    with prf.phase("test-outer"):
-        f()
-        with prf.phase("test-inner"):
-            f()
-        with prf.phase("test-inner2"):
-            f()
-
-    prf.write_to_file()
-
-
 
 def test_timer_funct():
     prf = Profiler()
 
     with prf.phase("test"):
         f()
-    assert "test" in prf._times
 
-    t1 = prf._times["test"]
-    with prf.phase("test"):
-        f()
-    t2 = prf._times["test"]
-    assert t1 < t2
-
-    with prf.phase("test-outer"):
-        f()
-        with prf.phase("test-inner"):
-            f()
-        with prf.phase("test-inner2"):
-            f()
-
-    assert prf._times["test-inner"] + prf._times["test-inner2"] < prf._times["test-outer"]
-
-def test_profiler_funct():
-    prf = Profiler()
-
-    with prf.phase("test"):
-        f()
-    assert "test" in prf._profiles
-    assert type(prf._profiles["test"]) == type(cProfile.Profile())
-
-    with prf.phase("test-outer"):
-        f()
-        with prf.phase("test-inner"):
-            f()
-        with prf.phase("test-inner2"):
-            f()
+    assert True
 
 
-debug()
+def test_write_file():
+    assert True
--- hatchet/util/profiler.py
+++ hatchet/util/profiler.py
@@ -10,144 +10,109 @@ from io import StringIO
 
 import cProfile
 import pstats
-import io
 
 
-# class for profiling
-class Profiler:
+class Timer(object):
+    """Simple phase timer with a context manager."""
+
     def __init__(self):
-        self._profile_stack = []
+        self._phase = None
+        self._start_time = None
         self._times = OrderedDict()
-        self._profiles = OrderedDict()
 
-    def start(self, phase):
+    def start_phase(self, phase):
+        now = datetime.now()
+        delta = None
+
+        if self._phase:
+            delta = now - self._start_time
+            self._times[self._phase] = delta
+
+        self._phase = phase
+        self._start_time = now
+        return delta
+
+    def end_phase(self):
+        assert self._phase and self._start_time
+
+        now = datetime.now()
+        delta = now - self._start_time
+        if self._times.get(self._phase):
+            self._times[self._phase] = self._times.get(self._phase) + delta
+        else:
+            self._times[self._phase] = delta
+
+        self._phase = None
+        self._start_time = None
+
+    def __str__(self):
+        out = StringIO()
+        out.write("Times:\n")
+        for phase, delta in self._times.items():
+            out.write("    %-20s %.2fs\n" % (phase + ":", delta.total_seconds()))
+        return out.getvalue()
+
+    @contextmanager
+    def phase(self, name):
+        self.start_phase(name)
+        yield
+        self.end_phase()
+
+
+# Wrapper class around cProfile
+# Exports a pstats file for
+#  reading by hpctoolkit
+class Profiler:
+    """
+        Wrapper class around cProfile.
+        Exports a pstats file to be read by hpctoolkit.
+    """
+
+    def __init__(self):
+        self._prf = cProfile.Profile()
+        self._output = "htrun.prof"
+
+    def start(self):
         """
             Description: Place before the block of code to be profiled.
-            Argument - phase (string): The name associated with this profile attempt
+            Argument - phase (string): The name associated with this profile
+            attempt
         """
-        prf = None
-
-        if phase in self._profiles:
-            prf = self._profiles[phase]
-        else:
-            prf = cProfile.Profile()
-
-        prf.enable()
-        self._profile_stack.append((prf, phase))
+        self._prf.enable()
 
     def end(self):
         """
             Description: Place at the end of the block of code being profiled.
             Return: The total runtime of the last profile attempt
         """
-        prf, phase = self._profile_stack.pop(-1)
-        prf.disable()
-        self._profiles[phase] = prf
-        self._times[phase] = pstats.Stats(prf).__dict__["total_tt"]
+        self._prf.disable()
 
-        return pstats.Stats(prf).__dict__["total_tt"]
+    def reset(self):
+        """
+            Description: Resets the profilier.
+        """
+        self._prf = cProfile.Profile()
 
     def __str__(self):
-        out = StringIO()
-        out.write("Times:\n")
-        for phase, delta in self._times.items():
-            out.write("    %-20s %.2fs\n" % (phase + ":", delta))
-        return out.getvalue()
-
-    # sorting options
-    # 'calls', 'cumulative', 'filename', 'ncalls', 'pcalls', 'line', 'name', 'nfl' (name file line), 'stdname', 'time'
-    def write_to_file(self, sortby="cumulative", filename="prof.txt"):
         """
-            Description: Dump the total statistics from this current profile organized
-            by phase
+            Description: Writes stats object out as a string.
         """
+        return pstats.Stats(self._prf).print_stats()
 
-        with open(filename, "w") as f:
-            pass
-
-        for profile in self._profiles:
-            with open(profile+"_temp.txt", "w") as f:
-                sts = pstats.Stats(self._profiles[profile], stream=f)
-                sts.sort_stats(sortby)
-                sts.print_stats()
-                # sts.print_callers()
-            with open(profile+"_temp.txt", "r") as f:
-                lines = f.readlines()
-
-            output = []
-            for i, line in enumerate(lines):
-                if line == '\n':
-                    continue
-                elif i == 0 or i == 2:
-                    output.append("# " + line)
-                elif i == 4:
-                    cols = line.split()
-                    cols.insert(0, "phase")
-                    line = "\t".join(cols) + '\n'
-                    output.append(line)
-                else:
-                    cols = line.split()
-                    new_cols = []
-                    for i in range(len(cols)):
-                        if cols[i] == "{":
-                            single_name = ""
-                            single_name += cols[i]
-                            i += 1
-                            while cols[i] != "}":
-                                single_name += cols[i] + " "
-                            single_name += cols[i]
-                            new_cols.append(single_name)
-                        else:
-                            new_cols.append(cols[i])
-                    new_cols.insert(0,profile)
-                    line = "\t".join(new_cols) + '\n'
-                    output.append(line)
-
-            with open(filename, "a") as f:
-                f.writelines(output)
-
-
-
-
-
+    def write_to_file(self, filename=""):
+        """
+            Description: Write the pstats object to a binary
+            file to be read in by an appropriate source.
+        """
+        sts = pstats.Stats(self._prf)
+        if filename == "":
+            sts.dump_stats(self._output + ".prof")
+        else:
+            sts.dump_stats(filename)
 
     @contextmanager
-    def phase(self, name):
-        self.start(name)
+    def phase(self, output):
+        self._output = output
+        self.start()
         yield
         self.end()
-
-    # def calcAvgStatsHelper(self, obj, num_of_runs):
-    #     for stat in obj:
-    #         lst = []
-    #         for val in range(0, 4):
-    #             if val < 2:
-    #                 var = int(obj[stat][val]) // num_of_runs
-    #                 lst.append(var)
-    #             else:
-    #                 lst.append(obj[stat][val] / num_of_runs)
-    #         if len(obj[stat]) > 4:
-    #             lst.append(obj[stat][4])
-    #
-    #         obj[stat] = tuple(lst)
-    #         if len(obj[stat]) > 4 and obj[stat][4] is not {}:
-    #             self.calcAvgStatsHelper(obj[stat][4], num_of_runs)
-
-    # def dumpAverageStats(
-    #     self, sortby="cumulative", filename="avg_prof.txt", num_of_runs=1
-    # ):
-    #     """
-    #         Description: Dump the average runtime statistics from this current profile to a
-    #         file. Cleared by calls to reset. All runtimes and statistics for every function call
-    #         is averaged over num_of_runs.
-    #     """
-    #     with open(filename, "w") as f:
-    #         f.write("\n\n Averaged over {} trials \n\n".format(num_of_runs))
-    #         sts = pstats.Stats(self.prf, stream=f)
-    #         if num_of_runs != 1:
-    #             self.calcAvgStatsHelper(sts.__dict__["stats"], num_of_runs)
-    #             sts.__dict__["total_tt"] = sts.__dict__["total_tt"] / num_of_runs
-    #             sts.__dict__["total_calls"] = sts.__dict__["total_calls"] // num_of_runs
-    #             sts.__dict__["prim_calls"] = sts.__dict__["prim_calls"] // num_of_runs
-    #         sts.sort_stats(sortby)
-    #         sts.print_stats()
